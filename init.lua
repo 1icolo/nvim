4260c4288bf2d5b8997f61612c194cac1608fd54
Nico Gabo <1icolo@proton.me>
2024-03-08 23:43:40 +0800

    nerdfont as optional

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 require 'options'
