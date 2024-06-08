3ead72924b42cd17efcea6b31565adab13bb8ba8
Nico Gabo <1icolo@proton.me>
2024-06-08 22:40:24 +0800

    removed unnecessary code from colorschemes

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ require 'lazy-bootstrap'
 require 'lazy-plugins'
 
 -- [[ Set colorscheme ]]
-vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme 'rose-pine'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/colorschemes.lua
+++ lua/colorschemes.lua
@@ -1,4 +1,4 @@
-IsTransparent = false
+IsTransparent = true
 IsTerminalColors = true
 GetCodeStyle = {
   comments = 'italic',
@@ -18,176 +18,29 @@ return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      -- flavour = "frappe", -- latte, frappe, macchiato, mocha
-      -- background = {      -- :h background
-      --     light = "latte",
-      --     dark = "mocha",
-      -- },
-      transparent_background = IsTransparent, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = IsTerminalColors, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = 'dark',
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { GetCodeStyle.comments }, -- Change the style of comments
-        conditionals = { 'italic' },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
-      color_overrides = {},
-      custom_highlights = {},
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = true,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
-        },
-      },
-    },
+    -- priority = 1000,
   },
   {
     'navarasu/onedark.nvim',
     name = 'onedark',
-    opts = {
-      -- Main options --
-      style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = IsTransparent, -- Show/hide background
-      term_colors = IsTerminalColors, -- Change terminal color as per the selected theme style
-      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-      -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
-
-      code_style = {
-        comments = GetCodeStyle.comments,
-        keywords = GetCodeStyle.keywords,
-        functions = GetCodeStyle.functions,
-        strings = GetCodeStyle.strings,
-        variables = GetCodeStyle.variables,
-      },
-      lualine = {
-        transparent = IsTransparent, -- lualine center bar transparency
-      },
-
-      -- Custom Highlights --
-      -- colors = {},     -- Override default colors
-      -- highlights = {}, -- Override highlight groups
-
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = true, -- use background color for virtual text
-      },
-    },
+    -- priority = 1000,
   },
   {
     'folke/tokyonight.nvim',
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {},
-    init = function()
-      -- Load the colorscheme here
-      -- vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
-    end,
+    name = 'tokyonight',
+    -- priority = 1000,
   },
   {
     'rose-pine/neovim',
+    priority = 1000,
     name = 'rose-pine',
     opts = {
-      variant = 'auto', -- auto, main, moon, or dawn
-      dark_variant = 'main', -- main, moon, or dawn
-      dim_inactive_windows = false,
-      extend_background_behind_borders = true,
-
       enable = {
         terminal = IsTerminalColors,
-        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-        migrations = true, -- Handle deprecated options automatically
       },
-
       styles = {
-        bold = true,
-        italic = true,
         transparency = IsTransparent,
       },
-
-      groups = {
-        border = 'muted',
-        link = 'iris',
-        panel = 'surface',
-
-        error = 'love',
-        hint = 'iris',
-        info = 'foam',
-        note = 'pine',
-        todo = 'rose',
-        warn = 'gold',
-
-        git_add = 'foam',
-        git_change = 'rose',
-        git_delete = 'love',
-        git_dirty = 'rose',
-        git_ignore = 'muted',
-        git_merge = 'iris',
-        git_rename = 'pine',
-        git_stage = 'iris',
-        git_text = 'rose',
-        git_untracked = 'subtle',
-
-        h1 = 'iris',
-        h2 = 'foam',
-        h3 = 'rose',
-        h4 = 'gold',
-        h5 = 'pine',
-        h6 = 'foam',
-      },
-
-      highlight_groups = {
-        -- Comment = { fg = "foam" },
-        -- VertSplit = { fg = "muted", bg = "muted" },
-      },
-
-      before_highlight = function(group, highlight, palette)
-        -- Disable all undercurls
-        -- if highlight.undercurl then
-        --     highlight.undercurl = false
-        -- end
-        --
-        -- Change palette colour
-        -- if highlight.fg == palette.pine then
-        --     highlight.fg = palette.foam
-        -- end
-      end,
     },
-
-    -- vim.cmd 'colorscheme rose-pine'
-    -- vim.cmd("colorscheme rose-pine-main")
-    -- vim.cmd("colorscheme rose-pine-moon")
-    -- vim.cmd("colorscheme rose-pine-dawn")
   },
 }
